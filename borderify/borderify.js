e8ed91c4b3552e57a02e6478dac1c62c39abf00c
A-tak <hoge+git@a-tak.com>
2020-02-02 15:42:46 +0900

    初めての拡張機能をお試し

---
+++ borderify/borderify.js
@@ -0,0 +1,5 @@
+// document.body.style.border = "15px solid blue";
+
+// Javascriptのセレクタ
+// https://public-constructor.com/css-javascript-html-selector/
+document.querySelector('h3').style.border = "15px solid blue";
